59e7a6001cb9dbb6b39a0b68622d8fe48e76abb3
b rad c <bradcorlett@gmail.com>
2024-09-28 18:20:12 -0700

    checkpoint

---
+++ templates/html/msample/sample_item/index.js
@@ -0,0 +1,47 @@
+// data functions //
+
+function verifySampleItem(data) {
+    /*
+
+    return {
+        valid: true/false,
+        errors: {
+            field: 'error message',
+            ...
+        }
+    }
+
+    */
+
+}
+
+function sampleItemFromInputElements(inputs) {   
+    // inputs is an object mapping field names to html input elements
+    // return data object of values
+}
+
+function sampleItemToInputElements(data) {
+    // return an object mapping field names to html input elements with values set
+}
+
+function sampleItemToDisplayElements(data) {
+    // return an object mapping field names to html elements with values set
+}
+
+// client functions //
+
+function createSampleItem(data) {
+
+}
+
+function readSampleItem(id) {
+
+}
+
+function updateSampleItem(id, data) {
+
+}
+
+function deleteSampleItem(id) {
+
+}
