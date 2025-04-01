96d5a67d26b3355cdc7ffaa155fb0947620a0535
b rad c <bradcorlett@gmail.com>
2025-03-31 23:43:42 -0700

    rename line buffer

--- src/lingo/gui.py
+++ src/lingo/gui.py
@@ -26,50 +26,33 @@ class LingoPage(tkinter.Frame):
     def __init__(self, parent): 
         super().__init__(parent)
 
-        self._line_open = False
-        self._line_buffer = None
-        self._line_first_row = -1
-        self._line_row_count = 0
-        self._line_tags = []
+        self._text_buffer_open = False
+        self._text_buffer = None
+        self._text_first_row = -1
+        self._text_row_count = 0
 
         self.app = lingo_app(example_spec)
         self.render_document()
 
     def _render_line_buffer(self):
-        if False:
-            line = tkinter.Label(self, text=self._line_buffer, font=TEXT)
-            line.grid(row=self._line_buffer_row, column=0, padx=0)
-        else:
-            #line = tkinter.Text(self, font=TEXT, wrap='word', height=1 * self._line_row_count, width=100, highlightthickness=0)
-            
-            #line.insert(tkinter.END, self._line_buffer)
-            self._line_buffer.grid(row=self._line_buffer_row, column=0, padx=0)
-
-            
-
-            # for n, tag in enumerate(self._line_tags):
-            #     try:
-            #         line.tag_add(tag['tag'], f'{tag["start"]}.0', f'{tag["end"]}.0')
-            #     except tkinter.TclError as e:
-            #         print(f"tag error: {e}")
-            #         breakpoint()
-            self._line_buffer.tag_configure('link', foreground='blue', underline=1)
-            self._line_buffer.config(state=tkinter.DISABLED)
+        self._text_buffer.grid(row=self._line_buffer_row, column=0, padx=0)
+        self._text_buffer.tag_configure('link', foreground='blue', underline=1)
+        self._text_buffer.config(state=tkinter.DISABLED)
 
     def _open_line_buffer(self, row:int):
-        if not self._line_open:
-            self._line_open = True
-            self._line_buffer = tkinter.Text(self, font=TEXT, wrap='word', height=10, width=100, highlightthickness=0)
+        if not self._text_buffer_open:
+            self._text_buffer_open = True
+            self._text_buffer = tkinter.Text(self, font=TEXT, wrap='word', height=10, width=100, highlightthickness=0)
             self._line_buffer_row = row
         else:
-            self._line_row_count += 1
+            self._text_row_count += 1
 
     def _close_line_buffer(self):
-        if self._line_open:
+        if self._text_buffer_open:
             self._render_line_buffer()
-            self._line_open = False
-            self._line_first_row = -1
-            self._line_row_count = 0
+            self._text_buffer_open = False
+            self._text_first_row = -1
+            self._text_row_count = 0
 
     def render_document(self):
         doc = render_document(self.app)
@@ -98,18 +81,12 @@ class LingoPage(tkinter.Frame):
         label.grid(row=row, column=0) 
 
     def render_text(self, row:int, element:dict):
-        # text = tkinter.Label(self, text=element['text'], font=TEXT)
-        # text.grid(row=row, column=0, padx=0)
         self._open_line_buffer(row)
-        # self._line_buffer += element['text']
-        self._line_buffer.insert(tkinter.END, element['text'])
+        self._text_buffer.insert(tkinter.END, element['text'])
 
     def render_break(self, row:int, element:dict):
-        # self._close_line_buffer()
-        # break_ = tkinter.Label(self, text='', font=TEXT)
-        # break_.grid(row=row, column=0, padx=0)
         self._open_line_buffer(row)
-        self._line_buffer.insert(tkinter.END, '\n' * element['break'])
+        self._text_buffer.insert(tkinter.END, '\n' * element['break'])
 
     def render_button(self, row:int, element:dict):
         self._close_line_buffer()
@@ -121,27 +98,14 @@ class LingoPage(tkinter.Frame):
         self.render_text(row, {'text': '* input place holder *'})
 
     def render_link(self, row:int, element:dict):
-        # self._close_line_buffer()
         try:
             display_text = element['text']
         except KeyError:
             display_text = element['link']
 
-        # link_label = ttk.Label(self, text=display_text, foreground='blue', cursor='hand2')
-        # link_label.bind("<Button-1>", lambda e: print(f"Link clicked: {element['link']}"))
-        # link_label.grid(row=row, column=0, padx=5)
-
         self._open_line_buffer(row)
-        self._line_buffer.insert(tkinter.END, display_text, ('link',))
+        self._text_buffer.insert(tkinter.END, display_text, ('link',))
 
-        # start_index = len(self._line_buffer)
-        # self._line_tags.append({
-        #     'tag': 'link', 
-        #     'start': start_index + 1, 
-        #     'end': start_index + len(display_text), 
-        # })
-
-        # self._line_buffer += display_text
 
 
 class LingoGUIApp(tkinter.Tk):
