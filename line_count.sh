f67099a09de20d0ccb150b31b57628c945f9e289
b rad c <bradcorlett@gmail.com>
2025-09-29 21:38:34 -0700

    version 0.0.9a

--- line_count.sh
+++
@@ -1,6 +0,0 @@
-find ./test-gen \
-    -type d -name .venv -prune -o \
-    -type d -name node_modules -prune -o \
-    -type d -name playwright-report -prune -o \
-    -type d -name test-results -prune -o \
-    -type f -exec cat {} + | wc -l
