855e2b9a48909986243ff9cd6ab5f1311b7706d2
b rad c <bradcorlett@gmail.com>
2025-11-01 20:14:16 -0700

    break core.go apart into context.go and types.go

--- templates/go/mapp/types.go
+++ templates/go/mapp/types.go
@@ -2,38 +2,9 @@ package mapp
 
 import (
 	"fmt"
-	"os"
 	"strings"
 	"time"
 )
-
-//
-// context
-//
-
-type Context struct {
-	ClientHost string `json:"client_host,omitempty"`
-	DBFile     string `json:"db_file,omitempty"`
-}
-
-var MappClientHostDefault = "http://localhost:5005"
-var MappDBFileDefault = "db.sqlite3"
-
-func ContextFromEnv() *Context {
-	clientHost := os.Getenv("MAPP_CLIENT_HOST")
-	if clientHost == "" {
-		clientHost = MappClientHostDefault
-	}
-	dbFile := os.Getenv("MAPP_DB_FILE")
-	if dbFile == "" {
-		dbFile = MappDBFileDefault
-	}
-
-	return &Context{
-		ClientHost: clientHost,
-		DBFile:     dbFile,
-	}
-}
 
 //
 // custom error
