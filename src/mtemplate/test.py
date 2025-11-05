f39b75323af9c2803a2a5b7711dabf1427ed7d0f
b rad c <bradcorlett@gmail.com>
2025-11-05 13:11:45 -0800

    add cli db tests

--- src/mtemplate/test.py
+++ src/mtemplate/test.py
@@ -1,10 +1,25 @@
 import unittest
+import json
 import subprocess
 
 from pathlib import Path
 from mspec.core import load_generator_spec
 
+def example_from_model(model:dict, index=0) -> dict:
+    data = {}
+    for field_name, field in model.get('fields', {}).items():
+        try:
+            value = field['examples'][index]
+        except (IndexError, KeyError):
+            raise ValueError(f'No example for field "{model["name"]["pascal_case"]}.{field_name}" at index {index}')
+        
+        data[field_name] = value
+
+    return data
+
 class TestMTemplateApp(unittest.TestCase):
+    
+    maxDiff = None
 
     """
     Test suite for the mtemplate app generation spec
@@ -25,9 +40,10 @@ class TestMTemplateApp(unittest.TestCase):
     cmd: list[str]
     host: str | None
 
-    def _run_cmd(self, cmd:list[str]) -> subprocess.CompletedProcess:
+    def _run_cmd(self, cmd:list[str], expected_code=0) -> subprocess.CompletedProcess:
         result = subprocess.run(cmd, capture_output=True, text=True)
-        self.assertEqual(result.returncode, 0, f'exit {result.returncode} for command "{" ".join(cmd)}" output: {result.stdout + result.stderr}')
+        msg = f'expected {expected_code} got {result.returncode} for command "{" ".join(cmd)}" output: {result.stdout + result.stderr}'
+        self.assertEqual(result.returncode, expected_code, msg)
         return result
 
     def test_help_menus(self):
@@ -41,12 +57,7 @@ class TestMTemplateApp(unittest.TestCase):
 
         # module help #
 
-        try:
-            modules: dict = self.spec['modules']
-        except KeyError:
-            raise ValueError('No modules found in spec for help menu testing')
-
-        for module in modules.values():
+        for module in self.spec['modules'].values():
             for module_help_arg in ['help', '--help', '-h']:
                 module_help_cmd = self.cmd + [module['name']['kebab_case'], module_help_arg]
                 result = self._run_cmd(module_help_cmd)
@@ -71,7 +82,53 @@ class TestMTemplateApp(unittest.TestCase):
             raise ValueError('No default_host found in spec and no host provided for testing')
 
     def test_cli_db_commands(self):
-        pass
+        for module in self.spec['modules'].values():
+            for model_name, model in module['models'].items():
+                model_db_args = self.cmd + [module['name']['kebab_case'], model['name']['kebab_case'], 'db']
+
+                # create #
+
+                example_to_create = example_from_model(model)
+                result = self._run_cmd(model_db_args + ['create', json.dumps(example_to_create)])
+                created_model = json.loads(result.stdout)
+
+                created_model_id = created_model.pop('id')  # remove id for comparison
+                self.assertEqual(created_model, example_to_create, f'Created {model_name} does not match example data')
+
+                # read #
+
+                result = self._run_cmd(model_db_args + ['read', str(created_model_id)])
+                read_model = json.loads(result.stdout)
+                read_model_id = read_model.pop('id')
+                self.assertEqual(read_model, example_to_create, f'Read {model_name} does not match example data')
+                self.assertEqual(read_model_id, created_model_id, f'Read {model_name} ID does not match created ID')
+
+                # update #
+
+                try:
+                    updated_example = example_from_model(model, index=1)
+                except ValueError as e:
+                    raise ValueError(f'Need at least 2 examples for update testing: {e}')
+                result = self._run_cmd(model_db_args + ['update', created_model_id, json.dumps(updated_example)])
+                updated_model = json.loads(result.stdout)
+                updated_model_id = updated_model.pop('id')
+                self.assertEqual(updated_model, updated_example, f'Updated {model_name} does not match updated example data')
+                self.assertEqual(updated_model_id, created_model_id, f'Updated {model_name} ID does not match created ID')
+
+                # delete #
+
+                result = self._run_cmd(model_db_args + ['delete', str(created_model_id)])
+                delete_output = json.loads(result.stdout)
+                self.assertEqual(delete_output['id'], created_model_id, f'Deleted {model_name} ID does not match created ID')
+                self.assertEqual(delete_output['message'], f'deleted {model["name"]["lower_case"]} {created_model_id}')
+
+                # read after delete #
+
+                result = self._run_cmd(model_db_args + ['read', str(created_model_id)], expected_code=1)
+                read_output = json.loads(result.stdout)
+                self.assertEqual(read_output['code'], 'not_found', f'Read after delete for {model_name} did not return not_found code')
+                self.assertEqual(read_output['message'], f'{model["name"]["lower_case"]} {created_model_id} not found', f'Read after delete for {model_name} did not return correct message')
+
 
     def test_cli_http_commands(self):
         pass
