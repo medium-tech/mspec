8efe3cfdb55f4f62d10a0f5f85f23f5a17c783f3
b rad c <bradcorlett@gmail.com>
2025-11-21 19:41:22 -0800

    add validation

--- src/mapp/errors.py
+++ src/mapp/errors.py
@@ -12,3 +12,13 @@ class MappError(Exception):
                 'message': self.error_message
             }
         }
+    
+class MappValidationError(MappError):
+    def __init__(self, message: str, field_errors:dict):
+        super().__init__('VALIDATION_ERROR', message)
+        self.field_errors = field_errors
+
+    def to_dict(self) -> dict:
+        error_dict = super().to_dict()
+        error_dict['error']['field_errors'] = self.field_errors
+        return error_dict
--- src/mapp/module/model/model.py
+++ src/mapp/module/model/model.py
@@ -1,4 +1,8 @@
 from collections import namedtuple
+from typing import Any
+
+from mapp.errors import MappValidationError
+
 
 def new_model_class(model_spec:dict) -> type:
     """
@@ -17,7 +21,9 @@ def new_model_class(model_spec:dict) -> type:
     except KeyError as e:
         raise ValueError(f'Missing required model specification key: {e}')
     
-    return namedtuple(class_name, fields)
+    new_class = namedtuple(class_name, fields)
+    new_class._model_spec = model_spec
+    return new_class
 
 def new_model(model_class:type, data:dict):
     """
@@ -53,4 +59,176 @@ def convert_data_to_model(model_class:type, data:dict):
     Returns:
         object: An instance of the model class.
     """
-    return new_model(model_class, data)
+
+    converted_data = {}
+
+    for field in model_class._model_spec['fields'].values():
+
+        field_name = field['name']
+        field_type = field['type']
+
+        try:
+            raw_value = data[field_name]
+        except KeyError:
+            """
+            ignore this error, this function only converts provided fields,
+            it does not validate the data
+            """
+            continue
+
+        try:
+            if isinstance(raw_value, list):
+                converted_data[field_name] = [convert_value(field_type, v) for v in raw_value]
+            else:
+                converted_data[field_name] = convert_value(field_type, raw_value)
+
+        except (ValueError, TypeError) as e:
+            raise ValueError(f'Error converting field "{field_name}" to type "{field_type}": {e}')
+
+    return new_model(model_class, converted_data)
+
+
+def convert_value(field_type:str, raw_value:Any, strict=False) -> Any:
+    """
+    Converts a raw value to the specified field type.
+    Args:
+        field_type (str): The target field type as a string.
+        raw_value (Any): The raw value to convert.
+        strict (bool): Whether to enforce strict conversion for booleans.
+            If strict is false, the following strings will be converted to bool:
+            - True values: 'true', 't', '1', 'yes', 'on'
+            - False values: 'false', 'f', '0', 'no', 'off
+    Returns:
+        Any: The converted value.
+    Raises:
+        ValueError: If the conversion fails or the field type is unsupported.
+    """
+
+    match field_type:
+        case 'bool':
+            if isinstance(raw_value, str):
+                if not strict and raw_value.lower() in ('true', 't', '1', 'yes', 'on'):
+                    return True
+                elif not strict and raw_value.lower() in ('false', 'f', '0', 'no', 'off'):
+                    return False
+                else:
+                    raise ValueError(f'Cannot convert string "{raw_value}" to bool')
+            else:
+                return bool(raw_value)
+        case 'int':
+            return int(raw_value)
+        case 'float':
+            return float(raw_value)
+        case 'str':
+            return str(raw_value)
+        case _:
+            raise ValueError(f'Unsupported field type: {field_type}')
+
+def get_python_type(field_type:str) -> type:
+    """
+    Maps a field type string to the corresponding Python type.
+
+    Args:
+        field_type (str): The field type as a string.
+
+    Returns:
+        type: The corresponding Python type.
+
+    Raises:
+        ValueError: If the field type is unsupported.
+    """
+
+    match field_type:
+        case 'bool':
+            return bool
+        case 'int':
+            return int
+        case 'float':
+            return float
+        case 'str':
+            return str
+        case _:
+            raise ValueError(f'Unsupported field type: {field_type}')
+
+def validate_model(model_class:type, model_instance:object) -> tuple[bool, dict[str|str]]:
+    """
+    Validates a model instance against its model class specification.
+
+    Args:
+        model_class (type): The model class.
+        model_instance (object): The model instance to validate.
+        
+        
+    Returns:
+        The model instance if it is valid.
+
+
+    Raises:        
+        MappValidationError: If the model instance is invalid, containing a dictionary of error messages if any. 
+            For any fields with validation errors, the dictionary will contain the field name as the key
+            and the first error message for that field as the value.
+    """
+
+    errors = {}
+    total_errors = 0
+
+    for field in model_class._model_spec['fields'].values():
+
+        # field definition #
+
+        field_name = field['name']
+        field_type = field['type']
+        required = field.get('required', False)
+
+        # get value #
+
+        try:
+            value = getattr(model_instance, field_name)
+        except AttributeError:
+            if required:
+                errors[field_name] = f'Field "{field_name}" is missing from the model instance.'
+                total_errors += 1
+            continue
+
+        #
+        # validate type
+        #
+
+        if field_type != 'list':
+
+            # confirm value type #
+
+            python_type = get_python_type(field_type)
+
+            if not isinstance(value, python_type):
+                errors[field_name] = f'Field "{field_name}" is not of type "{field_type}".'
+                total_errors += 1
+
+        else:
+            
+            # confirm is list type #
+
+            if not isinstance(value, list):
+                errors[field_name] = f'Field "{field_name}" is expected to be a list.'
+                total_errors += 1
+                continue
+
+            # element type definition #
+
+            try:
+                element_type = field['element_type']
+            except KeyError:
+                raise ValueError(f'Field "{field_name}" of type "list" is missing required "element_type".')
+            
+            python_type = get_python_type(element_type)
+
+            # confirm type of elements #
+
+            for i, element in enumerate(value):
+                if not isinstance(element, python_type):
+                    errors[field_name] = f'Element {i} of field "{field_name}" is not "{element_type}".'
+                    total_errors += 1
+                    break
+
+    if total_errors > 0:
+        raise MappValidationError('Model validation failed.', errors)
