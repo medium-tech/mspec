a984775119c0a766c604d73e80457702b79b676e
b rad c <bradcorlett@gmail.com>
2025-12-24 21:25:14 -0800

    remove widgets.js

--- browser2/js/src/widgets.js
+++
@@ -1,7 +0,0 @@
-/**
- * Create form from field specifications
- */
-function createFormFromFields(fieldSpec, fieldValues) {
-    // This function is currently unused but kept for future use
-    return null;
-}
