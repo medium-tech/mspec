9f5608535f12627059fee78f6789080076df0c10
b rad c <bradcorlett@gmail.com>
2026-03-15 22:59:06 -0700

    update comments in http op

--- src/mapp/module/op/http.py
+++ src/mapp/module/op/http.py
@@ -15,7 +15,10 @@ __all__ = [
 
 def http_run_op(ctx: MappContext, params_class:type, output_class:type, params:object) -> object:
 
-	# init #
+	#
+	# init
+	#
+
 	module_kebab = params_class._module_spec['name']['kebab_case']
 	op_kebab = params_class._op_spec['name']['kebab_case']
 
@@ -24,15 +27,25 @@ def http_run_op(ctx: MappContext, params_class:type, output_class:type, params:o
 
 	file_input = ctx.self.get('file_input', None)
 
-	# send request #
 	try:
+		
+		#
+		# file upload - multipart request
+		#
+
 		if file_input is not None:
+
+			# manually construct multipart request, this currently works but
+			# may need to be replaced with something like urllib3.encode_multipart_formdata
+			# for standard compliance
+
 			boundary = f'----MappBoundary{uuid.uuid4().hex}'
 			headers = ctx.client.headers.copy()
 			headers['Content-Type'] = f'multipart/form-data; boundary={boundary}'
 
 			# Prepare multipart body
 			parts = []
+
 			# JSON part
 			parts.append(f'--{boundary}\r\nContent-Disposition: form-data; name="json"\r\nContent-Type: application/json\r\n\r\n{json_request_body.decode()}\r\n')
 
@@ -42,7 +55,6 @@ def http_run_op(ctx: MappContext, params_class:type, output_class:type, params:o
 			mime_type = mimetypes.guess_type(filename)[0] or 'application/octet-stream'
 			parts.append(f'--{boundary}\r\nContent-Disposition: form-data; name="file"; filename="{filename}"\r\nContent-Type: {mime_type}\r\n\r\n')
 			parts[-1] = parts[-1].encode() + file_input + b'\r\n'
-
 
 			# End boundary
 			parts.append(f'--{boundary}--\r\n')
@@ -57,15 +69,24 @@ def http_run_op(ctx: MappContext, params_class:type, output_class:type, params:o
 
 			request = Request(url, headers=headers, method='POST', data=body)
 		else:
+
+			#
+			# regular JSON request
+			#
+
 			request = Request(url, headers=ctx.client.headers, method='POST', data=json_request_body)
+		
+		#
+		# send request and handle response
+		#
 
 		with urlopen(request) as response:
-
-			# Check if Content-Disposition is an attachment
-
 			content_disposition = response.getheader('Content-Disposition', '')
 
 			if content_disposition.startswith('attachment'):
+
+				# response is a file download #
+
 				try:
 					filename = content_disposition.split('filename=')[-1].strip('"')
 				except IndexError:
@@ -76,6 +97,9 @@ def http_run_op(ctx: MappContext, params_class:type, output_class:type, params:o
 				return new_op_output(output_class, {'result': {'acknowledged': True, 'message': f'Retrieved as: {filename}'}})
 			
 			else:
+
+				# response is a regular JSON response #
+
 				response_body = response.read().decode('utf-8')
 				return json_to_op_output(response_body, output_class)
 
