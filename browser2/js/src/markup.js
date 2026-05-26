be4b40324151522e2d04dfce0b02f60e6df01e7b
b rad c <bradcorlett@gmail.com>
2026-05-25 21:11:40 -0700

    update block rendering to align react buttons on single row

--- browser2/js/src/markup.js
+++ browser2/js/src/markup.js
@@ -2137,7 +2137,7 @@ function renderOutput(app, ctx = null) {
  */
 function renderBlock(app, element, ctx = null) {
     const elements = [];
-    // console.log('renderBlock()', element);
+    console.log('renderBlock()', element);
     for (let n = 0; n < element.block.length; n++) {
         const childElement = element.block[n];
         try {
@@ -4420,7 +4420,10 @@ function createDOMElement(app, element, ctx = null) {
         return createViewerElement(app, element, ctx);
     } else if (Array.isArray(element)) {
         // If the element is an array, render each item and wrap in a div
+		console.log('createDOMElement - rendering block element with', element.length, 'sub-elements');
         const container = document.createElement('div');
+		// container.className = 'block-list-rendering';
+		container.style.display = 'inline-block';
         for (const subElement of element) {
             // call createDomElement recursively for each subElement
             const childDom = createDOMElement(app, subElement, ctx);
