98ef13bbcb149e308d2bbc938c8d0c46bb43bfc3
b rad c <bradcorlett@gmail.com>
2026-07-28 23:11:01 -0700

    add lingo style types

--- lingo/interpreters/py/src/lingolib/types.py
+++ lingo/interpreters/py/src/lingolib/types.py
@@ -59,3 +59,47 @@ def value_to_str(value:LingoLiteralTypes) -> str:
 
 def error_to_str(error:LingoLanguageError) -> str:
 	return f'LINGO_ERROR [{error.code}] - {error.error}'
+
+def LingoStyleColors(StrEnum):
+
+	# shades of gray
+	white = 'white'
+	light_gray = 'lightgray'
+	gray = 'gray'
+	dark_gray = 'darkgray'
+	black = 'black'
+
+	# roygbiv
+	red = 'red'
+	orange = 'orange'
+	yellow = 'yellow'
+	green = 'green'
+	blue = 'blue'
+	indigo = 'indigo'
+	violet = 'violet'
+
+	# cmyk
+	cyan = 'cyan'
+	magenta = 'magenta'
+	# yellow = 'yellow' - defined above
+	# black = 'black' - defined above
+
+LingoStyleColorsList = [color.value for color in LingoStyleColors]
+
+def LingoStyleBlock(NamedTuple):
+	bold: bool = False
+	italic: bool = False
+	underline: bool = False
+	color: str = 'black'
+
+	def validate(self):
+		if not isinstance(self.bold, bool):
+			raise ValueError(f'Invalid value for bold: {self.bold}')
+		if not isinstance(self.italic, bool):
+			raise ValueError(f'Invalid value for italic: {self.italic}')
+		if not isinstance(self.underline, bool):
+			raise ValueError(f'Invalid value for underline: {self.underline}')
+		if not isinstance(self.color, str):
+			raise ValueError(f'Invalid value for color: {self.color}')
+		if self.color not in LingoStyleColorsList:
+			raise ValueError(f'Invalid color: {self.color}')
